365e291fc46e2486f8ad67bc81da5c1b65ffab8b
DereckGeorge <eridericgeorge@gmail.com>
2023-08-31 10:19:26 +0300

    Linked the app with firebase for otp verification

--- sevan_app/lib/main.dart
+++ sevan_app/lib/main.dart
@@ -5,8 +5,11 @@ import 'package:app/providers/mchezo_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
 import 'pages/Login_page/login_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
